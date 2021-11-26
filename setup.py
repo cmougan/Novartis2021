d8be93b397d6bb0acac3d713d10293f5e59ce2fc
Marc Fuster Rullan <marcfusterr@gmail.com>
2021-11-26 21:47:22 +0100

    settings to debug

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='metrics',
+   version='1.0',
+   description='A useful module',
+   author='Man Foo',
+   author_email='foomail@foo.com',
+   packages=['metrics'],  #same as name
+)
