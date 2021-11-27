8d689a25a04742a7d175c983eff2f8a85dd93e76
david26694 <david26694@gmail.com>
2021-11-27 10:03:31 +0100

    More preprocess tools

--- tools/impute.py
+++ tools/impute.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
 
 class PandasSimpleImputer(SimpleImputer):
     """A wrapper around `SimpleImputer` to return data frames with columns.
@@ -18,7 +19,18 @@ class PandasSimpleImputer(SimpleImputer):
             columns.append(f"col_{i}_imputed")
         return pd.DataFrame(transformed, columns=columns)
 
+class PandasStandardScaler(StandardScaler):
+    """A wrapper around `StandardScaler` to return data frames with columns.
+    """
 
-print(
-    PandasSimpleImputer(strategy="median", add_indicator=True).fit_transform(pd.DataFrame(dict(a=[1, None, 3], b=[None, 5, 6], c=[7, 8, 9])))
-)
+    def fit(self, X, y=None):
+        self.columns = X.columns
+        return super().fit(X, y)
+
+    def transform(self, X):
+        return pd.DataFrame(super().transform(X), columns=self.columns)
+
+
+# print(
+#     PandasSimpleImputer(strategy="median", add_indicator=True).fit_transform(pd.DataFrame(dict(a=[1, None, 3], b=[None, 5, 6], c=[7, 8, 9], d=["a", "b", "c"])))
+# )
