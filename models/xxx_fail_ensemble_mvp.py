28a676268c5a1f543a40e8099d165b64c57557e5
david26694 <david26694@gmail.com>
2021-12-09 15:37:21 +0100

    Improve code

--- models/xxx_fail_ensemble_mvp.py
+++ models/xxx_fail_ensemble_mvp.py
@@ -24,39 +24,52 @@ ground_truth_val = df_full.query("validation == 1").loc[:, ["month", "region", "
 sales_train = pd.read_csv("../data/data_raw/sales_train.csv")
 
 class Submission(object):
-    def __init__(self, name):
+    def __init__(
+        self,
+        name,
+        keys=('sales', 'lower', 'upper'),
+        alphas=(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
+    ):
         self.name = name
         self.val_df = pd.read_csv(f"../data/validation/{self.name}_val.csv")
         self.val_df = clip_first_month(self.val_df)
         self.submission_df = pd.read_csv(f"../submissions/{self.name}.csv")
         self.submission_df = clip_first_month(self.submission_df)
+        self.keys = keys
+        self.alphas = alphas
     
+    def __repr__(self):
+        return self.val_df.head(5).to_string()
+
     def __add__(self, other):
         output = deepcopy(self)
-        output.val_df['sales'] += other.val_df['sales']
-        output.submission_df['sales'] += other.submission_df['sales']
-
-        output.val_df['lower'] += other.val_df['lower']
-        output.submission_df['lower'] += other.submission_df['lower']
-
-        output.val_df['upper'] += other.val_df['upper']
-        output.submission_df['upper'] += other.submission_df['upper']
+        for key in self.keys:
+            output.submission_df[key] += other.submission_df[key]
+            output.val_df[key] += other.val_df[key]
 
         return output
 
     def __mul__(self, other: float):
         output = deepcopy(self)
-        output.val_df['sales'] *= other
-        output.submission_df['sales'] *= other
-
-        output.val_df['lower'] *= other
-        output.submission_df['lower'] *= other
-
-        output.val_df['upper'] *= other
-        output.submission_df['upper'] *= other
+        for key in self.keys:
+            output.submission_df[key] *= other
+            output.val_df[key] *= other
 
         return output
     
+    def optimize(self, other):
+        min_alpha = 0
+        best_submission = self
+        for alpha in self.alphas:
+            submission = (self * (1 - alpha)) + (other * alpha)
+            if submission <= best_submission:
+                min_alpha = alpha
+                best_submission = submission
+        return best_submission, min_alpha
+        
+    def __le__(self, other):
+        return self.eval_preds()[1] <= other.eval_preds()[1]
+
     def eval_preds(self):
         return ComputeMetrics(self.val_df, sales_train, ground_truth_val)
 # %%
@@ -68,7 +81,7 @@ for file in files:
 # %%
 (
     (
-        submissions["linear_model_simple"] + submissions["linear_model_simple"]
+        submissions["gbm_time_evol"] + submissions["linear_model_simple"]
     ) * 0.5
 ).eval_preds()
 
@@ -76,3 +89,6 @@ for file in files:
 # %%
 submissions["linear_model_simple"].eval_preds()
 # %%
+submissions["linear_model_simple"].optimize(submissions["gbm_time_evol"])
+# %%
+submissions["linear_model_simple"]
